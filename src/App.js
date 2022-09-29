b0723a201b8300b88842410d960f1df9768845e7
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-09-29 12:27:43 +0600

    add mycard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+import Question from './components/Question/Question';
 import Wrestlings from './components/Wrestlings/Wrestlings';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header></Header>
       <Wrestlings></Wrestlings>
+      <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Question.css'
+
+const Question = () => {
+    return (
+        <div className='mt-5'>
+            <h1 class="text-center">Question and answer</h1>
+            <div className=''>
+            <h4><span class="text-primary">Question-1:</span> What is the difference between var Let Const?</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
