16e9ff226c35a7d452bbcd67919e4dc1c3526222
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-09-29 21:15:15 +0600

    Time in the break complete

--- src/components/AddCard/AddCard.js
+++ src/components/AddCard/AddCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddCard.css'
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -15,24 +15,27 @@ const AddCard = (props) => {
     for(const player of cart){
         total = total + player.time;
     }
+
+    const [brTime, setBrTime] = useState()
+
     return (
         <div className='mt-4'>
             <h5>Add A Break</h5>
             <div className='bg-light text-warning p-2 rounded d-flex justify-content-between'>
                 <div className='bg-white card-time'>
-                    <a href="/me">20<sub>s</sub></a>
+                <button onClick={() => setBrTime(20)} type="button" class="btn btn-white">20<sub>s</sub></button>
                 </div>
                 <div className='bg-white card-time'>
-                    <a href="/me">30<sub>s</sub></a>
+                <button onClick={() => setBrTime(30)} type="button" class="btn btn-white">30<sub>s</sub></button>
                 </div>
                 <div className='bg-white card-time'>
-                    <a href="/me">40<sub>s</sub></a>
+                <button onClick={() => setBrTime(40)} type="button" class="btn btn-white">40<sub>s</sub></button>
                 </div>
                 <div className='bg-white card-time'>
-                    <a href="/me">50<sub>s</sub></a>
+                <button onClick={() => setBrTime(50)} type="button" class="btn btn-white">50<sub>s</sub></button>
                 </div>
                 <div className='bg-white card-time'>
-                    <a href="/me">60<sub>s</sub></a>
+                <button onClick={() => setBrTime(60)} type="button" class="btn btn-white">60<sub>s</sub></button>
                 </div>
             </div>
 
@@ -44,7 +47,7 @@ const AddCard = (props) => {
                 </div>
                 <div className='bg-light text-warning p-2 rounded d-flex justify-content-between align-items-center exercise mt-3'>
                     <h5>Break time</h5>
-                    <h6>00 seconds</h6>
+                    <h6>{brTime} seconds</h6>
                 </div>
             </div>
 
